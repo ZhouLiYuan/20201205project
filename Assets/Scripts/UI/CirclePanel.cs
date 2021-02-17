930f6a4ecd7745969d6deb50c63c5f1daf785d6b
ZhouLiYuan <905121635@qq.com>
2021-02-18 00:28:17 +0800

    20210218圆盘脚本更新

--- Assets/Scripts/UI/CirclePanel.cs
+++ Assets/Scripts/UI/CirclePanel.cs
@@ -7,15 +7,21 @@ using UnityEngine.UI;
 
 public class CirclePanel : MonoBehaviour
 {
-    //直接把脚本挂在canvas上
-    //[SerializeField] private RectTransform panelRect;
+    //不能直接把脚本挂在canvas上，由于容易被连带误删，UI组件上一般不挂脚本
+    [SerializeField] private RectTransform panelRect;
+
     [SerializeField] private int elementCount;
     [SerializeField] private Camera screenCam;
 
     private RectTransform circlePanelRect;
     
-    //为什么初始值赋为-1（因为永远在计算index时用不上，并且可以用于重置状态？）
+    //初始值赋-1（在计算index时不会被算到，-1在这是用作为状态值来使用）
     private int m_index = -1;
+    //状态较多的情况用 枚举 和 switch 来做状态机（switch限制状态条件会比多重if更严谨 可读性更高）
+    //四个状态，1待机 2选择中 3没选中 4选中了（3 4像是分支）
+    private enum State { Standby,Selecting,NoSelect,OnSelected}
+    private State m_state = State.Standby;
+
 
     private void Start()
     {
@@ -32,27 +38,55 @@ public class CirclePanel : MonoBehaviour
 
     private void HandleInput()
     {
-        if (Input.GetKey(KeyCode.O)) { ChooseElement(); }
-
-        //协程调用(只有调用过ChooseElement()方法，m_index 才会变成-1以外的值，才会在按下按键并松开的时候满足，1没有按下按键，2 m_index不等于-1 这两个条件？)
-        else
+        switch (m_state)
         {
-            if (m_index != -1) { StartCoroutine(SelectOne()); }
+            //待机状态下，按下O，进入选择状态
+            case State.Standby:
+                if (Input.GetKey(KeyCode.O)){ ChoosingElement(); }
+                break;
+
+               //有效的选择，进入 已选择状态
+            case State.Selecting:
+                m_state = State.OnSelected;
+                break;
+
+                //无效选择，返回待机状态
+            case State.NoSelect:
+                m_state = State.Standby;
+                break;
+                //已经选择并执行对应功能（协程，UI变色）
+            case State.OnSelected:
+                StartCoroutine(SelectOne());
+                break;
+            default:
+                break;
         }
+
+
+        //if (m_state == State.Standby && Input.GetKey(KeyCode.O)) { ChooseElement(); }
+
+        
+        //else
+        //{
+        //    //协程调用(只有调用过ChooseElement()方法，m_index 才会变成-1以外的值，才会在按下按键并松开的时候满足，1没有按下按键，2 m_index不等于-1 这两个条件)
+        //    //if (m_index != -1) { StartCoroutine(SelectOne()); }
+        //    //状态为被选中时
+        //    if (m_state == State.OnSelected) { StartCoroutine(SelectOne()); }
+        //}
 
     }
 
-    private void ChooseElement()
+    private void ChoosingElement()
     {
         //激活面板
         circlePanelRect.gameObject.SetActive(true);
         //确认screen的点在Rect中？这个方法会受Rect的pivot（作为中心基准）的影响
-        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(circlePanelRect, Input.mousePosition, screenCam, out Vector2 outVec))
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(circlePanelRect, Input.mousePosition, screenCam, out Vector2 localPoint))
         {
             //用于判断位置的角度
             float m_degree;
 
-            ComputeMousePosition(outVec, out m_degree);
+            ComputeMousePosition(localPoint, out m_degree);
 
             //角度是1°=1f吗？
             float elementDegree = 360 / elementCount;
@@ -68,16 +102,26 @@ public class CirclePanel : MonoBehaviour
                 {
                     Debug.Log($"选中  第{i}个，范围：{currentDegree}~{nextElementDegree}");
                     m_index = i;
+
+                    m_state = State.Selecting;
+                    return;
+
                 }
-                //这种==写法有没有问题
-                //else if (m_degree == currentDegree || m_degree == nextElementDegree) { }
-                else { m_index = -1; Debug.Log("无效选择范围"); }//这样就可以让区间 范围外的位置选择无效化？
+
+                //else { m_index = -1; Debug.Log("无效选择范围"); }//这样就可以让区间 范围外的位置选择无效化
+                m_state = State.NoSelect;
             }
 
 
         }
     }
 
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="v2"></param>
+    /// <param name="degree"></param>
     private void ComputeMousePosition(Vector2 v2, out float degree)
     {
         //默认中心pivot是canvas的pivot？
@@ -104,8 +148,9 @@ public class CirclePanel : MonoBehaviour
         //重置m_index
         m_index = -1;
 
-        //关闭圆盘
+        //关闭圆盘 并 重置状态
         circlePanelRect.gameObject.SetActive(false);
+        m_state = State.Standby;
     }
 
 }
