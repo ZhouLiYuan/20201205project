8ba3a852f6f119a62bd13fb4c0d959b1c5b3d54b
ZhouLiYuan <905121635@qq.com>
2021-04-16 23:57:29 +0800

    Add:协程管理器

---
+++ Assets/Scripts/CoroutineManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineManager 
+{
+    //套娃不会引发递归吗？
+    internal class CoroutineRunner : MonoBehaviour { }
+
+    //静态构造函数
+    static CoroutineManager()
+    {
+        var obj = new GameObject();
+        m_coroutineRunner = obj.AddComponent<CoroutineRunner>();
+    }
+
+    internal static CoroutineRunner m_coroutineRunner;
+
+    //传入枚举器
+    public static void StarCoroutine(IEnumerator enumerator)
+    {
+        m_coroutineRunner.StartCoroutine(enumerator);
+    }
+
+}
