628bd6e27e10c9a7b7dc7a2b1e468b2727145233
ZhouLiYuan <905121635@qq.com>
2021-05-05 18:29:32 +0800

    方法配合修改成协程版本

--- Assets/Scripts/Module/Scene/StartMenuScene.cs
+++ Assets/Scripts/Module/Scene/StartMenuScene.cs
@@ -11,14 +11,14 @@ public class StartMenuScene : BaseScene
     /// </summary>
     readonly string sceneName = "StartMenu";
 
-    PanelManager panelManager;
+   
 
     public override void OnEnter()
     {
         //如果没有激活的scene的Name叫 Start 的，就加载一个
         if (SceneManager.GetActiveScene().name != sceneName)
         {
-            panelManager = new PanelManager();
+           
             SceneManager.LoadScene(sceneName);
             SceneManager.sceneLoaded += SceneLoaded;
         }
@@ -28,7 +28,7 @@ public class StartMenuScene : BaseScene
     private void SceneLoaded(Scene scene, LoadSceneMode loadSceneMode) 
     {
        
-        panelManager.Push(new StartMenuPanel());
+        UIManager.OpenPanelByCoroutine<StartMenuPanel>();
 
         SceneManager.SetActiveScene(scene);
 
@@ -39,7 +39,8 @@ public override void OnExit()
     {
         //最后解除订阅，防止重复订阅
         SceneManager.sceneLoaded -= SceneLoaded;
-        panelManager.PopAll();
+        //差一个关掉所有面板的方法
+        //panelManager.PopAll();
     }
 
 }
