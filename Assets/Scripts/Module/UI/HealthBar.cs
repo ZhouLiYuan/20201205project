076c9dccc5b57f173a8b0ee199902f85c9db06b2
ZhouLiYuan <905121635@qq.com>
2021-05-05 18:21:59 +0800

    实现血条跟随（对血条所在canvas的renderMode做一些设置）

--- Assets/Scripts/Module/UI/HealthBar.cs
+++ Assets/Scripts/Module/UI/HealthBar.cs
@@ -3,19 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+//实现血条跟随帮助文档https://github.com/YuzikiRain/Learn/blob/master/Unity/Tips/%E5%AE%9E%E7%8E%B0%E8%A1%80%E6%9D%A1%E8%B7%9F%E9%9A%8F.md
 
 /// <summary>
-/// 模块脚本 表现层和逻辑层的连接就是currenthealth变量
+/// 模块脚本 表现层和逻辑层的耦合为currenthealth变量
 /// </summary>
 public class HealthBar : MonoBehaviour
 {
     public Slider m_slider;
+
     //颜色渐变功能
     public Gradient gradient;
     public Image m_fill;
 
     public int maxHealth;
     public int currentHealth;
+
+    /// <summary>
+    /// 脚本挂载的 对象的 transform
+    /// </summary>
+    private Transform m_owner;
+
+    /// <summary>
+    /// 为脚本内m_owner字段传值
+    /// </summary>
+    /// <param name="owner"></param>
+    public void SetOwner(Transform owner) { this.m_owner = owner; }
 
     //这个感觉也可以交给构造函数来做吧,mono里怎么调用构造函数
     /// <summary>
@@ -31,6 +44,10 @@ public class HealthBar : MonoBehaviour
         m_fill.color = gradient.Evaluate(1f);
     }
 
+    /// <summary>
+    /// 逻辑层映射到表现层
+    /// </summary>
+    /// <param name="health"></param>
     public void SetHealthBar(int health)
     {
         m_slider.value = health;
@@ -38,5 +55,20 @@ public class HealthBar : MonoBehaviour
         m_fill.color = gradient.Evaluate(m_slider.normalizedValue);
     }
 
-   
+    private void Update()
+    {
+        if (!m_owner) return;
+
+        var m_height = 0f;
+        //Canvas设置为 Screen Space - Camera 的方法
+        Vector3 viewPos = Camera.main.WorldToScreenPoint(m_owner.position + new Vector3(0f, m_height, 0f));
+
+        //根据比率自动缩放 横轴纵轴？意思是相对长宽？
+        var m_scaler = FindObjectOfType<CanvasScaler>();
+        var widthRatio = m_scaler.referenceResolution.x / Camera.main.scaledPixelWidth;
+        var heightRatio = m_scaler.referenceResolution.y / Camera.main.scaledPixelHeight;
+        GetComponent<RectTransform>().anchoredPosition = new Vector2(viewPos.x * widthRatio, viewPos.y * heightRatio);
+    }
+
+
 }
