a163867582aecb0ddd46ad20a4fd66b4028eab31
ZhouLiYuan <905121635@qq.com>
2021-05-08 17:00:40 +0800

    追加addressable同步加载版本API Enemy基类改用构造函数初始化

---
+++ Assets/Scripts/AssetModule.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public static class AssetModule 
+{
+    public static T LoadAsset<T>(string path)
+    {
+        var handle = Addressables.LoadAssetAsync<T>(path);
+        //实现同步加载的API
+        return handle.WaitForCompletion();
+    }
+}
--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 
 public static class EnemyManager
 {
-    public static IEnumerator InstantiateEnemyByCoroutine<TEnemy>(System.Action<TEnemy> onInstantiated = null) where TEnemy : BaseEnemy, new() 
+    public static BaseEnemy SpawnEnemy(string name)
     {
-        //panel并不作为component，只是需要一个地方存放 脚本类实例，用new不用AddComponent
-        var enemy = new TEnemy();
-
-        ////用协程方式加载
-        var loadPrefabHandle = new WaitLoadAsset<GameObject>(enemy.Path);
-
-
-        System.Action<GameObject> instantiateEnemylAction = prefab =>
-        {
-            GameObject enemyGobj = Object.Instantiate(prefab);
-            //加载完面板后初始化面板脚本中的字段
-            //因为 类型名就是面板名，所以可以typeof(TPanel).Name
-            enemyGobj.name = typeof(TEnemy).Name;
-            enemy.Init(enemyGobj.name, enemyGobj);
-            
-        };
-
-        yield return loadPrefabHandle;
-        //return new WaitInstantiateEnemy<TEnemy>(loadPrefabHandle, instantiateEnemylAction, onInstantiated);
+        var prefab = AssetModule.LoadAsset<GameObject>($"prefab/{name}");
+        var obj = Object.Instantiate(prefab);
+        BaseEnemy enemy = new BaseEnemy(name, obj);
+        return enemy;
     }
+    //public static IEnumerator InstantiateEnemyByCoroutine<TEnemy>(System.Action<TEnemy> onInstantiated = null) where TEnemy : BaseEnemy, new() 
+    //{
+    //    //panel并不作为component，只是需要一个地方存放 脚本类实例，用new不用AddComponent
+    //    var enemy = new TEnemy();
+
+    //    ////用协程方式加载
+    //    var loadPrefabHandle = new WaitLoadAsset<GameObject>(enemy.Path);
+
+
+    //    System.Action<GameObject> instantiateEnemylAction = prefab =>
+    //    {
+    //        GameObject enemyGobj = Object.Instantiate(prefab);
+    //        //加载完面板后初始化面板脚本中的字段
+    //        //因为 类型名就是面板名，所以可以typeof(TPanel).Name
+    //        enemyGobj.name = typeof(TEnemy).Name;
+    //        enemy.Init(enemyGobj.name, enemyGobj);
+
+    //    };
+
+    //    yield return loadPrefabHandle;
+    //    //return new WaitInstantiateEnemy<TEnemy>(loadPrefabHandle, instantiateEnemylAction, onInstantiated);
+    //}
 }
