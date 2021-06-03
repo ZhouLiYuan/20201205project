95fbe30482df56dcb4899613fc980e54355b1a1b
ZhouLiYuan <905121635@qq.com>
2021-06-03 22:58:22 +0800

    Add:Entity         建立每个单体Gobj和逻辑脚本 的联系

---
+++ Assets/Scripts/Controller/Entity.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 建立每个单体Gobj（表现层）和脚本（逻辑层）之间的联系
+/// </summary>
+public class Entity
+{
+    public GameObject GameObject { get; private set;}
+    public Transform Transform { get; private set;}
+
+    internal Entity(GameObject obj) 
+    {
+        this.GameObject = obj;
+        this.Transform = obj.transform;
+    }
+}
