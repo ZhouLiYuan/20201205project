fd54e9a83057c3c6fc28e212d4eb5cf9b67e4cab
ZhouLiYuan <905121635@qq.com>
2021-06-04 14:29:50 +0800

    Add:追加InputSystem相关文件和         脚本（建立逻辑层表现层联系）

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class PlayerInput 
+{
+    private InputActionAsset inputActionAsset;
+    private string inputActionAssetName => "Assets/AssetBundles_sai/Input/PlayerInput.inputactions";
+
+    //按键
+    public InputAction Move;
+    public InputAction Lock;
+    public InputAction Hook;
+    public InputAction Jump;
+
+    public void InitInput() 
+    {
+        inputActionAsset = Object.Instantiate(GameAssetManager.LoadInputActionAsset(inputActionAssetName));
+
+        //[0]好像是 面板里Action Maps栏的index 第一个的话对应的就是adventureMode？
+        InputActionMap inputActionMap = inputActionAsset.actionMaps[0];
+        inputActionMap.Enable();
+        //下面是 在建立 表现层和逻辑层的耦合吗？
+
+        Move = inputActionMap.FindAction("Move");
+        Move.Enable();
+        Lock = inputActionMap.FindAction("Lock");
+        Lock.Enable();
+        Hook = inputActionMap.FindAction("Hook");
+        Hook.Enable();
+        Jump = inputActionMap.FindAction("Jump");
+        Jump.Enable();
+    }
+
+
+}
