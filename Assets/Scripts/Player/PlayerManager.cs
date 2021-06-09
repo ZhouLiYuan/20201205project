582e9f3889e1dd7c9d77016da9486f944decfae4
ZhouLiYuan <905121635@qq.com>
2021-06-09 11:00:44 +0800

    Update：支持多角色选择

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -3,21 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-//目前只有一个角色AkaOni所以，暂时不用泛型
+//支持多角色
 public static class PlayerManager
 {
+    private static List<PlayerRole> m_Roles = new List<PlayerRole>();
+
     /// <summary>
     /// 加载并生成 主角Gobj，并创建 主角脚本实例
     /// </summary>
     /// <returns></returns>
-    public static PlayerRole SpawnCharacter() 
+    public static PlayerRole SpawnCharacter(string roleName) 
     {
-        //获得表现层实例
-        var prefab = AssetModule.LoadAsset<GameObject>("Assets/AssetBundles_sai/character.prefab");
-        GameObject characterGobj = Object.Instantiate(prefab);
-
-        //获得脚本实例
-        PlayerRole m_Role = new PlayerRole(characterGobj);
+        var prefab = AssetModule.LoadAsset<GameObject>($"Assets/AssetBundles_sai/{roleName}.prefab");
+        //创建逻辑层和表现层实例
+        PlayerRole m_Role = new PlayerRole(Object.Instantiate(prefab));
+        m_Roles.Add(m_Role);
         return m_Role;
     }
 
