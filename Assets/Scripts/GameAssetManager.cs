15ba1fe188541e2247352204b9a62badb43563d7
ZhouLiYuan <905121635@qq.com>
2021-06-09 11:02:22 +0800

    Add：抽象层管理游戏资产

---
+++ Assets/Scripts/GameAssetManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.InputSystem;
+using UnityEngine;
+
+
+/// <summary>
+/// 再抽象多一层 游戏资源加载管理
+/// </summary>
+public class GameAssetManager
+{
+    public static InputActionAsset LoadInputActionAsset(string path)
+    {
+        return AssetModule.LoadAsset<InputActionAsset>(path);
+    }
+}
