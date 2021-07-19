f71c639a2c993d06e4c1625abc4e7d41cacffb31
ZhouLiYuan <905121635@qq.com>
2021-07-19 14:47:56 +0800

    Add: GameManager          管理GameController字段

---
+++ Assets/SAIGameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//抽象多一层GameController的管理层
+public static class SAIGameManager
+{
+    private static GameController m_gameController;
+
+    public static void StartGame<TGameController>(int level) where TGameController : GameController, new() 
+    {
+        m_gameController = new TGameController();
+        m_gameController.StartGame(level);
+    }
+
+    public static void ExitGame() 
+    {
+        m_gameController.ExitGame();
+    }
+
+}
