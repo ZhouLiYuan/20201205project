14230998a438ede967518a51157bacc16d1afc61
ZhouLiYuan <905121635@qq.com>
2021-08-01 08:45:22 +0800

    Update:修复platform不被记录在scriptable Object问题

--- Assets/Scripts/Data/PlatformInfo.cs
+++ Assets/Scripts/Data/PlatformInfo.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
 public class PlatformInfo 
 {
     public Vector3 Position;
