41cdd8efb1066b6a2e1fb240fdd2df0883e3092e
ZhouLiYuan <endlessunite@gmail.com>
2021-08-25 22:28:51 +0800

    Add:全局事件脚本

---
+++ Assets/Scripts/GlobalEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class GlobalEvent
+{
+    public static event Action<InteractableData> OnEnterInteractArea;
+    public static event Action<InteractableData> OnExitInteractArea;
+    public static event Action<int> OnShowDialogue;
+
+    public static event Action<UnityEngine.InputSystem.InputAction.CallbackContext> OnPressInteract;
+
+    public static void EnterInteractArea(InteractableData data) { OnEnterInteractArea?.Invoke(data); }
+    public static void ExitInteractArea(InteractableData data) { OnExitInteractArea?.Invoke(data); }
+    public static void ShowDialogue(int storyId) { OnShowDialogue?.Invoke(storyId); }
+
+    public static void Interact(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        OnPressInteract?.Invoke(obj);
+    }
+}
