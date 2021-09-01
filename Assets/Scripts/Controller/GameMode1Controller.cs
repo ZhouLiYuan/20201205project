349b94771dec91883099898a45cfda612cd465f4
ZhouLiYuan <endlessunite@gmail.com>
2021-09-02 00:28:10 +0800

    Update:追加 对话相关脚本的实例化和初始化(订阅事件)操作

--- Assets/Scripts/Controller/GameMode1Controller.cs
+++ Assets/Scripts/Controller/GameMode1Controller.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
+//订阅方法往往会在初始化方法中做
+//解除订阅一般在退出的时候做
 public class GameMode1Controller  : GameController
 {
     private GamePanel gamePanel;
 
     //private List<GameObject> enemyObjs = new List<GameObject>();
     private PlayerRole m_role;
+
+    Updater updater;
+    DialogSystem dialogueSystem;
+    InteractableManager interactableManager;
 
 
     //需要序列化的敌人名（可能之后需要抽象成一个节点？）
@@ -35,11 +40,24 @@ public class GameMode1Controller  : GameController
 
         //耦合Lock UI逻辑和 PlayerRole功能
         m_role.OnShowLockTarget += gamePanel.LockHint;
+
+        //初始化对话系统
+        var dialogueSystem = new DialogSystem();
+        dialogueSystem.Init();
+
+        updater = Updater.AddUpdater();
+        updater.AddUpdateFunction(dialogueSystem.OnUpdate);
+
+        interactableManager = new InteractableManager();
+        interactableManager.Init();
+
     }
 
     public override void ExitGame()
     {
         m_role.OnShowLockTarget -= gamePanel.LockHint;
+
+        updater.RemoveUpdateFunction(dialogueSystem.OnUpdate);
     }
 
 
