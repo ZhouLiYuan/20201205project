f9c961a3a359ea510f5bd79e9e92ad5862167862
ZhouLiYuan <endlessunite@gmail.com>
2021-10-02 21:15:16 +0800

    Add：一些测试备份文件

---
+++ Assets/Tester.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tester :MonoBehaviour
+{
+    private PlayerRole m_role;
+
+    Updater m_updater;
+
+    private void Start()
+    {
+        m_updater = Updater.AddUpdater();
+
+        //创建玩家 控制器
+        PlayerInput m_playerInput = new PlayerInput();
+        m_playerInput.InitInput();
+        //加载主角
+        m_role = PlayerManager.SpawnCharacter();
+        //控制器和角色耦合
+        m_role.BindInput(m_playerInput);
+
+        LevelManager.InitByConfiguration(0);
+    }
+}
