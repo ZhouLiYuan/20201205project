2583dbc9b829888a276e70c107dd7dd552428df9
ZhouLiYuan <endlessunite@gmail.com>
2021-10-14 23:19:36 +0800

    Add:Tag&LayerManager
    方便配置Tag，减少打错打多空格的bug

---
+++ Assets/Scripts/TagAndLayerManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagManager
+{
+    public static string Player => "Player";
+    public static string Enemy => "Enemy";
+
+    //功能尚未检验
+    public static string AddTag(string tagName)
+    {
+        for (int i = 0; i < UnityEditorInternal.InternalEditorUtility.tags.Length; i++)
+        {
+            if (UnityEditorInternal.InternalEditorUtility.tags[i].Contains(tagName)) return UnityEditorInternal.InternalEditorUtility.tags[i];
+        }
+        UnityEditorInternal.InternalEditorUtility.AddTag(tagName);
+        return tagName;
+    }
+}
+
+
+public class LayerManager
+{
+    public static string Player => "Player";
+    public static string Enemy => "Enemy";
+}
