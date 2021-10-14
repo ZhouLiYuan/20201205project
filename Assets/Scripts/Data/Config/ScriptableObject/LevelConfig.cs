43d7bdbe57fc8a00bbf32defb98a6b1fe5510fc8
ZhouLiYuan <endlessunite@gmail.com>
2021-10-14 22:11:00 +0800

    Update:整理合并配置属性脚本

---
+++ Assets/Scripts/Data/Config/ScriptableObject/LevelConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//LevelConfig成员的赋值在LevelEditorWindow中完成
+
+/// <summary>
+/// 关卡信息
+/// 目前只有 LevelConfig 信息来自于ScriptableObject
+/// </summary>
+public class LevelConfig : ScriptableObject
+{
+   
+    //值类型
+    public int levelID;
+    public string levelName;
+
+    //引用类型
+    public List<EnemyInfo> EnemyInfos = new List<EnemyInfo>();
+    public List<PlatformInfo> PlatformInfos = new List<PlatformInfo>();
+    public List<NPC> NPCs = new List<NPC>();
+    public List<CheckPointInfo> CheckPointInfos = new List<CheckPointInfo>();
+
+}
---
+++ Assets/Scripts/Data/InfoCollections.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+public class InfoCollections
+{
+
+}
+
+
+
+public class EntityInfo
+{
+    public Vector3 Position;
+    public int AssetID;
+}
+
+
+[Serializable]
+public class EnemyInfo : EntityInfo
+{
+    public int WeaponID;
+}
+
+
+[Serializable]
+public class PlatformInfo
+{
+    public string AssetName;
+}
+
+[Serializable]
+public class CheckPointInfo
+{
+
+}
