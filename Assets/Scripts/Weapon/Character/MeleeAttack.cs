7ae712993d616275964b617909a2676dfb57c5c1
ZhouLiYuan <endlessunite@gmail.com>
2021-10-21 00:07:22 +0800

    Update:无用方法注释

--- Assets/Scripts/Weapon/Character/MeleeAttack.cs
+++ Assets/Scripts/Weapon/Character/MeleeAttack.cs
@@ -1,81 +1,81 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class MeleeAttack : MonoBehaviour
-{
-    /// <summary>
-    /// 实时变化的攻击间隔
-    /// 第一次攻击不用等，所以初始值为0
-    /// </summary>
-    private float atkInterval = 0;
-    /// <summary>
-    /// 攻击间隔容量，是一个常量，用于重置atkInterval
-    /// 防止玩家连按，Tms的开始界面有不同的逻辑实现类似的功能
-    /// </summary>
-    public float atkIntervalVolume;
-    public float attackRange;
-    public int atkValue;
+//public class MeleeAttack : MonoBehaviour
+//{
+//    /// <summary>
+//    /// 实时变化的攻击间隔
+//    /// 第一次攻击不用等，所以初始值为0
+//    /// </summary>
+//    private float atkInterval = 0;
+//    /// <summary>
+//    /// 攻击间隔容量，是一个常量，用于重置atkInterval
+//    /// 防止玩家连按，Tms的开始界面有不同的逻辑实现类似的功能
+//    /// </summary>
+//    public float atkIntervalVolume;
+//    public float attackRange;
+//    public int atkValue;
 
-    public LayerMask enemiesLayer;
-    /// <summary>
-    /// 攻击范围中心点
-    /// </summary>
-    public Transform attackPos;
+//    public LayerMask enemiesLayer;
+//    /// <summary>
+//    /// 攻击范围中心点
+//    /// </summary>
+//    public Transform attackPos;
 
 
 
-    private void Update()
-    {
-        //之后可以放入inputHandler
-        if (Input.GetKey(KeyCode.J)) { Attack(); }  
-    }
+//    private void Update()
+//    {
+//        //之后可以放入inputHandler
+//        if (Input.GetKey(KeyCode.J)) { Attack(); }  
+//    }
 
-    void Attack()
-    {   
-        //如果想要cameraShake功能是不是要在 渲染相机上挂个animator k震动动画 然后再方法种调用？
+//    void Attack()
+//    {   
+//        //如果想要cameraShake功能是不是要在 渲染相机上挂个animator k震动动画 然后再方法种调用？
 
-        //当间隔冷却时间结束后可以开始下一次攻击
-        if (atkInterval <= 0)
-        {
+//        //当间隔冷却时间结束后可以开始下一次攻击
+//        if (atkInterval <= 0)
+//        {
 
-            //也许这块也可做成一个 可以敌人玩家都重复利用的模块？在敌人身上实验中
-            //一个CheckToAttack检测攻击范围内刚体对象
-            //一个GetDamage模块
+//            //也许这块也可做成一个 可以敌人玩家都重复利用的模块？在敌人身上实验中
+//            //一个CheckToAttack检测攻击范围内刚体对象
+//            //一个GetDamage模块
 
-            //获取检测范围内所有敌人的碰撞体(Overlap系列)
-            Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, enemiesLayer);
-            //若集中对象集合非空，则对敌人造成伤害
-            if (enemiesToDamage != null) 
-            {
-                for (int i = 0; i < enemiesToDamage.Length; i++)
-                {
-                    //通过collder组件 访问 所挂载Gobj的 其他组件
-                    enemiesToDamage[i].GetComponent<EnemyGetDamage>().TakeDamage(atkValue);
-                }
+//            //获取检测范围内所有敌人的碰撞体(Overlap系列)
+//            Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, enemiesLayer);
+//            //若集中对象集合非空，则对敌人造成伤害
+//            if (enemiesToDamage != null) 
+//            {
+//                for (int i = 0; i < enemiesToDamage.Length; i++)
+//                {
+//                    //通过collder组件 访问 所挂载Gobj的 其他组件
+//                    enemiesToDamage[i].GetComponent<EnemyGetDamage>().TakeDamage(atkValue);
+//                }
 
-                //item名是可以随便起的吗？
-                foreach (Collider2D enemy in enemiesToDamage)
-                {
-                    Debug.Log($" kick{enemy.name}'s ass!!");
-                }
+//                //item名是可以随便起的吗？
+//                foreach (Collider2D enemy in enemiesToDamage)
+//                {
+//                    Debug.Log($" kick{enemy.name}'s ass!!");
+//                }
 
-                //攻击结束后重置冷却时间
-                atkInterval = atkIntervalVolume;
-            }
-        }
-        else 
-        {   //继续恢复冷却时间
-            atkInterval -= Time.deltaTime;
-        }
-    }
+//                //攻击结束后重置冷却时间
+//                atkInterval = atkIntervalVolume;
+//            }
+//        }
+//        else 
+//        {   //继续恢复冷却时间
+//            atkInterval -= Time.deltaTime;
+//        }
+//    }
 
-    /// <summary>
-    /// 可视化验证攻击区域
-    /// </summary>
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(attackPos.position, attackRange);
-    }
-}
+//    /// <summary>
+//    /// 可视化验证攻击区域
+//    /// </summary>
+//    private void OnDrawGizmosSelected()
+//    {
+//        Gizmos.color = Color.blue;
+//        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+//    }
+//}
--- Assets/Scripts/Weapon/old/Gun.cs
+++ Assets/Scripts/Weapon/old/Gun.cs
@@ -1,85 +1,85 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-/// <summary>
-/// 名字可以替换成投射物  教程地址https://www.youtube.com/watch?v=wkKsl1Mfp5M&t=0s 16分50秒有讲解EffectLine的设置方法
-/// </summary>
-public class Gun : MonoBehaviour
-{
-    /// <summary>
-    /// 投射物发射点
-    /// </summary>
-    public Transform startPoint;
-    public GameObject bulletPrefab;
-    public GameObject laserPrefab;
-    public GameObject l_hitEffect;
+///// <summary>
+///// 名字可以替换成投射物  教程地址https://www.youtube.com/watch?v=wkKsl1Mfp5M&t=0s 16分50秒有讲解EffectLine的设置方法
+///// </summary>
+//public class Gun : MonoBehaviour
+//{
+//    /// <summary>
+//    /// 投射物发射点
+//    /// </summary>
+//    public Transform startPoint;
+//    public GameObject bulletPrefab;
+//    public GameObject laserPrefab;
+//    public GameObject l_hitEffect;
 
-    public LineRenderer m_lineRenderer;
+//    public LineRenderer m_lineRenderer;
 
 
-    private float atkInterval = 0;
-    public float atkIntervalVolume;
+//    private float atkInterval = 0;
+//    public float atkIntervalVolume;
    
-    /// <summary>
-    /// 镭射枪的伤害数值
-    /// </summary>
-    public int l_atkValue;
+//    /// <summary>
+//    /// 镭射枪的伤害数值
+//    /// </summary>
+//    public int l_atkValue;
 
 
-    private void Update()
-    {
-        //之后可以放入inputHandler
-        if (Input.GetKey(KeyCode.I)) { ShootBullet(); }
-        if (Input.GetKey(KeyCode.P)) { StartCoroutine( ShootLaser()); }
-    }
+//    private void Update()
+//    {
+//        //之后可以放入inputHandler
+//        if (Input.GetKey(KeyCode.I)) { ShootBullet(); }
+//        if (Input.GetKey(KeyCode.P)) { StartCoroutine( ShootLaser()); }
+//    }
 
-    void ShootBullet() 
-    {  
-        Instantiate(bulletPrefab, startPoint.position, startPoint.rotation);   
-        //具体伤害数值的实现交给 挂载再子弹上的脚本实现
-    }
+//    void ShootBullet() 
+//    {  
+//        Instantiate(bulletPrefab, startPoint.position, startPoint.rotation);   
+//        //具体伤害数值的实现交给 挂载再子弹上的脚本实现
+//    }
 
-    IEnumerator ShootLaser() 
-    {
+//    IEnumerator ShootLaser() 
+//    {
 
-        //hitInfo被击中物的信息
-        RaycastHit2D hitInfo = Physics2D.Raycast(startPoint.position, startPoint.right);
+//        //hitInfo被击中物的信息
+//        RaycastHit2D hitInfo = Physics2D.Raycast(startPoint.position, startPoint.right);
 
-        if (hitInfo)
-        {
-            Debug.Log(hitInfo.transform.name);
+//        if (hitInfo)
+//        {
+//            Debug.Log(hitInfo.transform.name);
 
-            //为什么找的是Enemy游戏组件，而不是敌人GOBJ本身？
-            Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
-            if (enemy!= null)
-            {
-                GetComponent<EnemyGetDamage>().TakeDamage(l_atkValue);
-            }
-            //生成被镭射击中的特效
-            Instantiate(l_hitEffect, hitInfo.point, Quaternion.identity);
+//            //为什么找的是Enemy游戏组件，而不是敌人GOBJ本身？
+//            Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
+//            if (enemy!= null)
+//            {
+//                GetComponent<EnemyGetDamage>().TakeDamage(l_atkValue);
+//            }
+//            //生成被镭射击中的特效
+//            Instantiate(l_hitEffect, hitInfo.point, Quaternion.identity);
 
-            //setPosition，两点确定一线，点之间有先后顺序，第一个参数是 绘制顺序 第二个是点的位置
-            m_lineRenderer.SetPosition(0, startPoint.position);
-            //在击中敌人的情况下，第二个点是敌人
-            m_lineRenderer.SetPosition(1, hitInfo.point);
+//            //setPosition，两点确定一线，点之间有先后顺序，第一个参数是 绘制顺序 第二个是点的位置
+//            m_lineRenderer.SetPosition(0, startPoint.position);
+//            //在击中敌人的情况下，第二个点是敌人
+//            m_lineRenderer.SetPosition(1, hitInfo.point);
 
-        }
-        else 
-        {
-            m_lineRenderer.SetPosition(0, startPoint.position);
-            //在没有击中敌人的情况下，第二个点是无限远
-            m_lineRenderer.SetPosition(1, startPoint.position + startPoint.right*100);
-        }
-
-
-        m_lineRenderer.enabled = true;
-        //用协程来延时射线的消失
-        //延时0.02秒
-        yield return 0.02;
-        m_lineRenderer.enabled = false;
-    }
+//        }
+//        else 
+//        {
+//            m_lineRenderer.SetPosition(0, startPoint.position);
+//            //在没有击中敌人的情况下，第二个点是无限远
+//            m_lineRenderer.SetPosition(1, startPoint.position + startPoint.right*100);
+//        }
 
 
+//        m_lineRenderer.enabled = true;
+//        //用协程来延时射线的消失
+//        //延时0.02秒
+//        yield return 0.02;
+//        m_lineRenderer.enabled = false;
+//    }
 
-}
+
+
+//}
