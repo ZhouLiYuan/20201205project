75057928d73f03e5a8e84a099cbbba16e79d5cca
ZhouLiYuan <endlessunite@gmail.com>
2021-10-23 01:23:15 +0800

    Add:添加攻击状态脚本

---
+++ Assets/Scripts/FSM/State/Player/Attack/GunAttackState.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+
+
+
+public class GunAttackState : PlayerRoleState
+{
+    public override void OnEnter()
+    {
+        base.OnEnter();
+    }
+
+    public override void OnUpdate(float deltaTime)
+    {
+        //横向输入 x轴方向
+        Velocity = new Vector2(InputAxis.x * MoveSpeed, Velocity.y);
+        //垂直方向 跳跃速度
+        if (Role.IsJumpPressed && Role.GroundDetect.IsGrounded) ChangeState<JumpState>();
+        if (Role.InvincibleTime == 0 && Role.isAttacked) ChangeState<DamagedState>();
+        //if (Velocity == Vector2.zero) ChangeState<IdleState>();
+    }
+
+    public override void OnExit()
+    {
+        //Role.animator.ResetTrigger("Move");
+    }
+}
