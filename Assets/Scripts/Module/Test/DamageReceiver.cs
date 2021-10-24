9ed8ceb2c108811f9dd54f3cb9ef886dfbbc2439
ZhouLiYuan <endlessunite@gmail.com>
2021-10-24 19:16:30 +0800

    Update：游戏机制中 敌人与玩家受伤逻辑优化
    避免一次攻击行为中 触发多次碰撞体碰撞
    敌人杂兵可以连续被伤害 不受硬直保护
    玩家不可被连续伤害，受到硬直保护

--- Assets/Scripts/Module/Test/DamageReceiver.cs
+++ Assets/Scripts/Module/Test/DamageReceiver.cs
@@ -23,7 +23,7 @@ public class DamageReceiver : MonoBehaviour
         //根据BaseDamageReceiver所在层级换API获取
         ownerGobj = transform.parent.gameObject;
         ownerCollider = transform.GetComponent<Collider2D>();
-        //ownerLayer = LayerMask.NameToLayer(LayerMask.LayerToName(ownerGobj.layer));
+        ownerLayer = 1 << ownerGobj.layer;
 
     }
 
@@ -40,9 +40,12 @@ public class DamageReceiver : MonoBehaviour
         GameObject attackerGobj = weaponGobj.transform.parent.parent.gameObject;
         LayerMask attackerLayer = attackerGobj.layer;
 
-        //碰撞后只要父级的图层并不是同一层，就可以相互伤害
-        if (colliderLayer == LayerMask.NameToLayer("Attacker") && attackerLayer != ownerLayer)
+        //碰撞后只要父级名称不同，就可以相互伤害(父级的图层并不是同一层才能受伤可以避免敌人友军伤害)
+        if (colliderLayer == LayerMask.NameToLayer("Attacker") && (attackerLayer != ownerGobj.layer) && attackerGobj.name!= ownerGobj.name)
         {
+            var spawnPos = (collision.transform.position + transform.position) / 2;//希望特效更靠近受击方
+            spawnPos.y = transform.position.y;
+
             //当攻击者是Enemy
             if (attackerLayer == LayerMask.NameToLayer("Enemy"))
             {
@@ -51,6 +54,7 @@ public class DamageReceiver : MonoBehaviour
                 attackerWeapon = attacker.availableWeapons[collision];
                 PlayerManager.m_Role.OnAttacked += attackerWeapon.ATK;
                 PlayerManager.m_Role.isAttacked = true;
+                if(PlayerManager.m_Role.InvincibleTime == 0) GenerateEffect(spawnPos);
             }
 
             //当攻击者是 Player
@@ -59,22 +63,37 @@ public class DamageReceiver : MonoBehaviour
                 //从receiver出发找
                 var enemyCollider = transform.gameObject.GetComponent<Collider2D>();
                 var enemy = EnemyManager.GetInstanceByCollider(enemyCollider);
-                if(enemy.IsCurrentHitOver)enemy.animator.SetTrigger("Damaged");
+                //if (enemy.IsCurrentHitOver) enemy.animator.SetTrigger("Damaged");
+                if (enemy.IsCurrentHitOver == false) return;
+                //避免在同一次攻击中发生连续碰撞
+                enemy.animator.SetTrigger("Damaged");
+                GenerateEffect(spawnPos);
+                enemy.IsCurrentHitOver = false;
             }
-            else { }
-
-            var spawnPos = (collision.transform.position + transform.position) / 2;//希望特效更靠近受击方
-            spawnPos.y = transform.position.y;
-            GenerateEffect(spawnPos);
+            else { return; }
+            
+            Debug.Log($"受击方{ownerGobj.name} 所在图层{LayerMask.LayerToName(ownerLayer)}  攻击碰撞体{collision.gameObject.name} 攻击方{attackerGobj.name}所在图层{LayerMask.LayerToName(attackerLayer)}");
+          
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.tag != TagManager.Player && collision.gameObject.tag != TagManager.Enemy) return;
-        PlayerManager.m_Role.OnAttacked -= attackerWeapon.ATK;
-        //这里不知道为啥没调用到，导致角色一直处于可以被伤害的状态
-        PlayerManager.m_Role.isAttacked = false;
+
+        if (ownerGobj.tag == TagManager.Player)
+        {
+            PlayerManager.m_Role.OnAttacked -= attackerWeapon.ATK;
+            //这里不知道为啥没调用到，导致角色一直处于可以被伤害的状态
+            PlayerManager.m_Role.isAttacked = false;
+        }
+        else if (ownerGobj.tag == TagManager.Enemy) 
+        {
+            var enemyCollider = transform.gameObject.GetComponent<Collider2D>();
+            var enemy = EnemyManager.GetInstanceByCollider(enemyCollider);
+            enemy.IsCurrentHitOver = true;
+        }
+
     }
 
     private void GenerateEffect(Vector3 spawnPos) 
