ef1c509f02479be1c0f3e96b9c12352ee4b8535a
ZhouLiYuan <endlessunite@gmail.com>
2021-10-30 14:44:29 +0800

    Update：追加二段跳功能

--- Assets/Scripts/FSM/State/Player/JumpState.cs
+++ Assets/Scripts/FSM/State/Player/JumpState.cs
@@ -2,13 +2,15 @@
 
 public class JumpState : PlayerRoleState
 {
+    private int jumpCount;//剩余跳跃次数
+
     public override void OnEnter() 
     {
         //Role.animator.SetTrigger("Jump");
         base.OnEnter();
         //垂直方向 跳跃速度
         Velocity = new Vector2(Velocity.x, JumpSpeed);
-
+        jumpCount = 1;
     }
 
     public override void OnUpdate(float deltaTime)
@@ -16,6 +18,11 @@ public class JumpState : PlayerRoleState
         //滞空时也可以缓慢移动
         Velocity = new Vector2(InputAxis.x * MoveSpeed/2, Velocity.y);
         if (Role.GroundDetect.IsGrounded) ChangeState<IdleState>();
+        if (Role.IsJumpTriggered && jumpCount > 0)//二段跳
+        {
+            Velocity = new Vector2(Velocity.x, JumpSpeed);
+            jumpCount--;
+        } 
         if (Role.isAttacked && Role.InvincibleTime <= 0) ChangeState<DamagedState>();
     }
 
