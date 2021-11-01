50aeb3de12b3e67b3a7789ef437b37185b6f91c1
ZhouLiYuan <endlessunite@gmail.com>
2021-11-01 22:49:24 +0800

    Update：完善抓取敌人功能

--- Assets/Scripts/FSM/State/Player/GetTargetState.cs
+++ Assets/Scripts/FSM/State/Player/GetTargetState.cs
@@ -21,23 +21,33 @@ public class GetTargetState : HookBaseState
         base.OnUpdate(deltaTime);
         TargetEnemy.animator.SetTrigger("Grabbed");
         if (!Role.IsHookPressing) ChangeState<PreSubActionState>();
-        direction = role_Gobj.transform.position - TargetPos;
+        direction = role_Gobj.transform.position - TargetEnemy.Transform.position;//指向Player的方向
 
         HookTransform.position = TargetEnemy.Transform.position;//hook黏在敌人身上
         //HookTransform.position = new Vector3(1.2f,0.5f,0f) + Role.Transform.position;//把hook想象成一个骨骼位置
         GenerateHookEffect(HookPos);
-        if (direction.sqrMagnitude > 1)//1是敌人离角色的最小距离
+
+        //敌人离角色的最小距离
+        var minColliderDistance = (Role.HitCollider.bounds.size + TargetEnemy.HitCollider.bounds.size) / 2;
+        if (direction.sqrMagnitude > minColliderDistance.sqrMagnitude +1)
         {
             //怎样做到速度合理渐变
-            //var k = Vector2.Distance(Role.Transform.position, TargetPos);
+            var k = Vector2.Distance(Role.Transform.position, TargetEnemy.Transform.position) -1;//-1减去玩家和敌人自身体型的半径和
+            if (k < 0) k = 0;
+            if (k > 1) k = 1;
+            Debug.Log($"斜率值{k} ");
+            grapSpeed = grapSpeed * k;
             //grapSpeed = Mathf.Lerp(grapSpeed, grapSpeed * k, k);
             //grapSpeed = Mathf.Lerp(grapSpeed, grapSpeed* 0.5f, deltaTime);
-            TargetEnemy.Transform.GetComponent<Rigidbody2D>().velocity = direction.normalized * grapSpeed;
+            TargetEnemy.rg2d.velocity = direction.normalized * grapSpeed;
+            Debug.Log($"抓取状态1方向{direction}最小间距 {minColliderDistance}");
         }
-        else//抓取物已到角色附近
+        else if(direction.sqrMagnitude <=  minColliderDistance.sqrMagnitude + 1)//这样写和只写else有什么区别（为什么写else就没反应？）
         {
-            TargetOwnerTransform.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-            TargetOwnerTransform.position = Role.Transform.position - direction.normalized;//-个offset
+            Debug.Log("抓取状态2");
+            //TargetOwnerTransform最好别用，因为未必拿到Hookable信标
+            TargetEnemy.Transform.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            TargetEnemy.Transform.position = Role.Transform.position - direction.normalized*1.3f;//+个offset(这个数值直接决定了能不能精确维持else if的状态)
             //TargetOwnerTransform.SetParent(HookTransform, true);
         }
     }
--- Assets/Scripts/Role/Enemy/FSM/EnemyGrabbedState.cs
+++ Assets/Scripts/Role/Enemy/FSM/EnemyGrabbedState.cs
@@ -9,20 +9,24 @@ public class EnemyGrabbedState : EnemyBaseState
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         //避免层级一些旋转轴问题，之后修改层级设计
-        en_AnimatorGobj = animator.gameObject;
-        en_TopNodeTransform = en_AnimatorGobj.transform;
+        en_TopNodeGobj = animator.gameObject;
         en_HitCollider = animator.transform.GetComponent<Collider2D>();
         Init();
+        en_rb2d.gravityScale = 0f;//处于失重状态
+        en_HitCollider.isTrigger = true;//无法与玩家产生实际碰撞
     }
 
 
     public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+    
         if (!PlayerManager.m_Role.IsHookPressing && animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1f) { animator.SetTrigger("Idle"); }//被抓硬直是动画播放时长
     }
 
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        en_HitCollider.isTrigger = false;
+        en_rb2d.gravityScale = 1f;//处于失重状态
         //animator.ResetTrigger(triggerName);
         animator.ResetTrigger("Grabbed");
         //Enemy.IsCurrentHitOver = true;
