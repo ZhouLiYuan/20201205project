6ead343bc79d34824d11c93f32e6e8ca441a7f4b
ZhouLiYuan <endlessunite@gmail.com>
2021-11-09 06:10:13 +0800

    Add：添加面具系统以及Asset

---
+++ Assets/Scripts/Weapon/RoleMask.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//主要是美术上的表现，没有实际功能（像一个换装功能），以及一个互动的部分
+//mask和weapon是配套的，可以做一对KeyValuePair
+public class RoleMask:Entity
+{
+    
+}
