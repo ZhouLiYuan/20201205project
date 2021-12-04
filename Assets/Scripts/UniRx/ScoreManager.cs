a515f8c00713228e202e9330b5020cd0a329e51f
ZhouLiYuan <endlessunite@gmail.com>
2021-12-04 22:48:06 +0800

    Add：评价模块，UniTask相关

---
+++ Assets/Scripts/UniRx/ScoreManager.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System.Linq;
+
+using TMPro;
+using UniRx;//计时器API也很好用
+
+//模仿鬼泣不同阶段评分(村上设置在吃到药丸时才会进入fever)
+//村上喜欢把带返回值的方法当成一个只读属性来用
+public enum ComboLabelType 
+{
+    C,B,A
+}
+
+
+//计分系统（以后可以搞个字典记录每关连击数据，给个评价）升级为评价系统evaluationSystem
+public class ScoreManager : SingletonMonoBehaviour<ScoreManager>
+{
+    //表现层演出部分
+
+    public int CurrentCombo => m_totalCombo.Value;
+    [SerializeField] Image currentComboLabel;
+    [SerializeField] TextMeshProUGUI currentComboText;
+
+    //目前 村上的方法 全部都需要在Inspector中手动赋值
+    //自处建议 1脚本写死 2写成ScriptableObject 在对应文件里配置好
+    [SerializeField] ComboViewConfig combo_C;
+    [SerializeField] ComboViewConfig combo_B;
+    [SerializeField] ComboViewConfig combo_A;
+
+    [SerializeField] ComboBorder[] m_comboBorders;//在Inspector中自行决定分界个数
+
+
+    //逻辑部分
+    const string ScoreSaveKey = "";
+
+
+    public bool IsHighScore => (m_totalScore.Value >= LoadScore());//是否刷新最高分
+
+    //UniRx自定义相应属性
+    IntReactiveProperty m_totalScore = new IntReactiveProperty(0);//initialValue最初值指的是什么的最初值？
+    public IReadOnlyReactiveProperty<int> TotalScore => m_totalScore;//Score变更通知
+    IntReactiveProperty m_totalCombo = new IntReactiveProperty(0);
+    public IReadOnlyReactiveProperty<int> TotalCombo => m_totalCombo;
+
+
+
+    //当前连招奖励倍率 (比如连续消灭十个敌人时，倍率为2，原本一个敌人奖励为100，现在变成200)
+    public float GetCurrentMagnification()
+    {
+        var element = m_comboBorders
+            .Where(x =>
+            {//x是ComboBorder类型的参数
+                if (m_comboBorders.LastOrDefault() == x)
+                { return x.BorderMax <= CurrentCombo; }
+                return x.BorderMin <= CurrentCombo &&
+                CurrentCombo <= x.BorderMax;//为什么中途是返回布尔值？又是怎么确认 x是ComboBorder类型的参数的？
+            }).FirstOrDefault();//返回第一个元素(或集合为空时返回默认值) 
+
+        return element != null ? element.Magnification : 1.0f;
+        //x?y:z x为 true 返回y |false 返回z
+    }
+
+    //根据玩家当前连招评价信息改变 评价的UI演出
+    public void ChangeComboLabel(ComboLabelType type)
+    {
+        switch (type)
+        {
+            case ComboLabelType.C:
+                ConfigConboLabel(combo_C);
+                break;
+            case ComboLabelType.B:
+                ConfigConboLabel(combo_B);
+                break;
+            case ComboLabelType.A:
+                ConfigConboLabel(combo_A);
+                break;
+            default:
+                break;
+        }
+    }
+    private void ConfigConboLabel(ComboViewConfig comboConfig)
+    {
+        currentComboLabel.sprite = comboConfig.ComboSprite;
+        currentComboLabel.color = comboConfig.ComboColor;
+        currentComboText.color = comboConfig.ComboColor;
+    }
+
+
+
+
+
+
+    #region Score相关方法:Add Save Load Reset Delete
+
+    //参数 击败当前敌人 获得的固有分数
+    //处理：最终加分=固有分数 x 当前连击倍率
+    public int AddScore(int score)
+    {
+        AddCombo();
+        m_totalScore.Value += (int)(score * GetCurrentMagnification());//增加总分
+        return (int)(score * GetCurrentMagnification());
+    }
+
+    public void ScoreSave()
+    {
+        if (m_totalScore.Value < LoadScore()) return;
+        PlayerPrefs.SetInt(ScoreSaveKey, m_totalScore.Value);
+        PlayerPrefs.Save();//直接存在磁盘中
+    }
+    //获取 玩家以往最高分
+    public int LoadScore()
+    {
+        //PlayerPrefs持久化储存，声明周期是多久，似乎Unity被关闭后重新打开依然存在
+        return PlayerPrefs.GetInt(ScoreSaveKey, 0);//貌似因为是储存最高分，所以只需要一个Key一个Value，
+    }
+    //清空（重置当前得分）
+    public void ResetScore()
+    {
+        m_totalScore.Value = 0;
+    }
+    //删除保存的得分记录
+    public void DeleteScore()
+    {
+        PlayerPrefs.DeleteKey(ScoreSaveKey);//删除键和匹配的值
+    }
+
+    #endregion
+
+
+
+    #region Combo相关方法:Add Clear
+    //感觉这种写法有点脱裤子放屁, 目的是从方法名 提高可读性？
+    public void AddCombo() { m_totalCombo.Value++; }
+
+    public void ResetCombo() { m_totalCombo.Value = 0; }
+
+    #endregion
+}
+
+
+[Serializable]
+public class ComboViewConfig
+{
+    [SerializeField] Sprite m_comboSprite;
+    [SerializeField] Color m_comboColor;
+
+    public Sprite ComboSprite => m_comboSprite;
+    public Color ComboColor => m_comboColor;
+}
+
+/// <summary>
+/// 连招评价变化边界值（也是倍率奖励的变化边界）
+/// </summary>
+[Serializable]
+public class ComboBorder
+{
+    [SerializeField] int m_borderMin = 0;
+    [SerializeField] int m_borderMax = 0;
+    [SerializeField] float m_magnification = 1.0f;//borderMin~borderMax区间范围内的奖励倍率
+
+    //确保了只能通过Inspector配置变量值
+    public int BorderMin => m_borderMin;
+    public int BorderMax => m_borderMax;
+    public float Magnification => m_magnification;
+}
---
+++ Assets/Scripts/UniRx/Unitask/ResultManager.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Linq;
+using TMPro;//71也有用
+using UniRx;//71也有用
+using Cysharp.Threading.Tasks;
+using App.Sound;
+
+
+//UniRx 在ver7.0后分离了两个模块UniRx和UniTask，UniRx.Async命名空间不复存在
+//(取而代之需要引用Cysharp.Threading.Tasks命名空间) 详见https://increment-log.com/unirx-async-separated-unitask/
+//API查找https://www.jianshu.com/p/94b857be40ec
+
+
+
+//关于异步较好的讲解https://www.cnblogs.com/zhaoshujie/p/11192036.html
+//知识点：1 从await后 同方法体内的代码在新开的线程上执行，然后跳出方法体后续代码会在异步方法执行的同时执行
+//             2 如果需要用到异步方法的返回值 或者 处理结果 那么要小心锁死或者空引用
+//异步更像是同时进行，协程则可暂时理解为是 加了阻塞功能 版本的异步， 在执行外部代码的同时，满足“yield return + 等待条件”中的条件后再执行后续代码
+//异步和回调关联强
+
+
+//每个关卡结束时的评价面板，暂时模仿鬼泣或者恶魔城( 应该弄一个ResultPanel)，目前暂定ResultManager挂在ResultPanel上，为button提供一些点击回调事件
+//成功闯关时才会出现的面板ResultPanel
+//与ScoreManager强关联
+public class ResultManager:SingletonMonoBehaviour<ResultManager>
+{
+    //---------------------<UI配置>--------------------
+    [SerializeField] TextMeshProUGUI m_scoreText;//后期应该全部写死在脚本里（先暂时用手动拖拽方式实现）
+    [SerializeField] TextMeshProUGUI m_bestScoreText;
+    //当Inspector中 需要多个变量又不用单个元素变量名时（适用于 集合内元素 处理比较统一的情况）
+    [SerializeField] Image[] m_icons;//面板上的UI图像
+
+    //-------------------<ResultPanel>---------------------
+    //动画用(目前暂定从下网上滚)
+    [SerializeField] RectTransform m_resultPanelTransform;
+    [SerializeField] float m_scrollSpeed = 30f;//满版滚动速度
+
+    [SerializeField] float m_feedoutTime = 0.5f;//场景切换速度
+    IDisposable m_fadeout;//接口的引用 貌似 不能作 未被赋值的本地变量（从头到尾没看懂这个引用有什么用）
+    float m_time = 0;
+
+    //-------------------<Unity生命周期函数>---------------------
+    void Start() 
+    {
+        gameObject.SetActive(false);
+    }
+
+
+    //-------------------<Button对应的回调方法>---------------------
+
+    //异步不是多线程(游戏不存在多线程)多线程只是异步的一种手段http://www.choupangxia.com/2021/02/15/async/
+    public async UniTask OnResultAsync() 
+    {
+        gameObject.SetActive(true);
+        SoundManager.Instance.StopBgm();
+
+        int score = ScoreManager.Instance.TotalScore.Value;//总分
+        m_scoreText.text = $"{score:#,0}";//后面的 :#,0 有什么作用？
+
+
+        int bestScore = ScoreManager.Instance.LoadScore();
+        m_bestScoreText.text = $"{bestScore:#,0}";
+
+        await ScrollUp();//在异步的同时，异步所在的新线程 while后的代码或下一轮的循环 都在每帧末执行
+    }
+
+    IEnumerator ScrollUp()
+    {
+        while (m_resultPanelTransform.position.y < 0) //村上的默认设置中Pos Y=-1920(隔了一整个游戏屏幕)
+        {
+            m_resultPanelTransform.Translate(0, Time.deltaTime * m_scrollSpeed, 0);
+            yield return new WaitForEndOfFrame();//暂时跳出ScrollUp()方法体外继续执行方法体外的代码，延时（阻塞）到每帧末才处理方法体内yield return的后续内容
+        }
+        m_resultPanelTransform.position = Vector3.zero;
+    }
+
+    public void OnContinue ()
+    {
+        SceneChange(m_feedoutTime);
+    }
+
+    public void OnBack()
+    {
+        SceneChange(m_feedoutTime);
+    }
+
+    //这个方法本身也没太看懂
+    public void SceneChange(float second) 
+    {
+        m_fadeout?.Dispose();//接口引用非空 配置？
+
+        var curve = new AnimationCurve();
+        curve.AddKey(0, 1);//API k帧
+        curve.AddKey(second, 0);
+
+        m_time = 0f;//重置计时
+        //FrameInterval和IntervalFrame不是一个东西
+        //实现接口引用
+        m_fadeout = Observable
+            .IntervalFrame(1)
+            .TakeWhile(_ => curve.Evaluate(m_time) > 0)//当参数为false时停止发射数据  ("_"是匿名方法的参数）
+            .Subscribe(_ =>//订阅了两个匿名回调方法 1,改变UI不透明度
+            {
+                m_time += Time.deltaTime;
+                var color = new Color(1, 1, 1, curve.Evaluate(m_time));//用 曲线key(即time)对应的value变化 来控制透明度
+
+                foreach (var icon in m_icons)
+                {
+                    icon.color *= color;//*=目的是为了只改变 透明度参数（icon颜色默认透明度也是1）
+                }
+                m_scoreText.color *= color;
+                m_bestScoreText.color *= color;
+            }
+            ,
+            () =>
+            {
+                m_fadeout.Dispose();//非空检测开头做过了（但依然不知道是什么时候赋值的。。。。）
+                m_fadeout = null;
+
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);//重新加载当前场景（场景加载可以因需求而异）
+            })
+            .AddTo(this);//貌似是把组件实例配置到重新加载的场景中？
+
+    }
+
+}
