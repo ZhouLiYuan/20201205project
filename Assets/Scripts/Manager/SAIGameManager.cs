1d2070acb65a46ce1bfaa188bb346becbc613887
ZhouLiYuan <endlessunite@gmail.com>
2021-12-18 02:41:55 +0800

    Update:BTL模式配套API追加

--- Assets/Scripts/Manager/SAIGameManager.cs
+++ Assets/Scripts/Manager/SAIGameManager.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿
 //抽象多一层GameController的管理层
 public static class SAIGameManager
 {
@@ -12,10 +9,16 @@ public static class SAIGameManager
     /// </summary>
     /// <typeparam name="TGameController"></typeparam>
     /// <param name="level"></param>
-    public static void StartGame<TGameController>(int level) where TGameController : GameController, new() 
+    public static void StartAdvGame<TGameController>(int level) where TGameController : GameController, new() //用于ADV
     {
         m_gameController = new TGameController();
         m_gameController.StartGame(level);
+    }
+
+    public static void StartBtlGame<TGameController>(int P1_characterID = 0, int P2_characterID = 1) where TGameController : GameController, new()//用于BTL
+    {
+        m_gameController = new TGameController();
+        m_gameController.StartGame(P1_characterID, P2_characterID);
     }
 
     public static void ExitGame() 
